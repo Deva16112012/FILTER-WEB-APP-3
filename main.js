d6f73cf286222e90999f7af490583e0553d49a8d
Deva16112012 <100427772+Deva16112012@users.noreply.github.com>
2022-11-30 15:28:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function Lipstick(){
+    window.location = "index_lipstick.html";
+}
+
+function Mustach(){
+    window.location = "index_mustach.html";
+}
---
+++ main__mustach.js
@@ -0,0 +1,36 @@
+nose_x=0;
+nose_y=0;
+function preload(){
+    x=loadImage('mustach.png');
+}
+
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poses=ml5.poseNet(video,modelloded);
+    poses.on('pose',kp);
+}
+
+function draw(){
+    image(video,0,0,300,300);
+    image(x,nose_x,nose_y,40,40);
+}
+
+function modelloded(){
+    console.log("pose net is loded");
+}
+
+function kp(r){
+    if(r.length>0){
+        console.log(r);
+        nose_x=r[0].pose.nose.x-20;
+        nose_y=r[0].pose.nose.y+5;
+    }
+}
+
+function take_snapshot(){
+    save('Mustach.png');
+}
